1393333a29ffcbfea8ca99b8743be3d64c92f25c
Ujjwols <shrestha.ujjwol1@gmail.com>
2025-07-03 02:29:59 +0545

    delployable

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/Portfolioo/" : "/",
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
   plugins: [
     react(),
